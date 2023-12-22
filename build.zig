77e79c36e1443b0911df8e26d77ee777dade0fa0
Jeremy Daffon <jdaffon123@gmail.com>
2023-12-22 00:33:02 -0800

    Test readding back hzzp module

--- build.zig
+++ build.zig
@@ -4,14 +4,15 @@ pub fn build(b: *Builder) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const hzzp_dep = b.dependency("hzzp", .{});
+    const hzzp_mod = b.addModule("hzzp", .{ .source_file = .{
+        .path = hzzp_dep.builder.pathFromRoot(hzzp_dep.module("hzzp").source_file.path),
+    } });
     const unit_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
-    unit_tests.addAnonymousModule("hzzp", .{
-        .source_file = .{ .cwd_relative = hzzp_dep.builder.pathFromRoot("src/main.zig") },
-    });
+    unit_tests.addModule("hzzp", hzzp_mod);
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&unit_tests.step);
 }
