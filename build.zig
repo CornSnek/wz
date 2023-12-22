8b92ff792575b0aef2a41ddb69e5286f3f731902
Jeremy Daffon <jdaffon123@gmail.com>
2023-12-21 23:10:47 -0800

    Testing zon without using modules

--- build.zig
+++ build.zig
@@ -4,18 +4,14 @@ pub fn build(b: *Builder) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const hzzp_dep = b.dependency("hzzp", .{});
-    const hzzp_mod = b.addModule("hzzp", .{ .source_file = .{
-        .path = hzzp_dep.builder.pathFromRoot(hzzp_dep.module("hzzp").source_file.path),
-    } });
-    _ = b.addModule("wz", .{
-        .source_file = .{ .path = "src/main.zig" },
-    });
     const unit_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
-    unit_tests.addModule("hzzp", hzzp_mod);
+    unit_tests.addAnonymousModule("hzzp", .{
+        .source_file = .{ .cwd_relative = hzzp_dep.builder.pathFromRoot("src/main.zig") },
+    });
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&unit_tests.step);
 }
